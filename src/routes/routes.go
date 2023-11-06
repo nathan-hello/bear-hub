088f6c89943d2618e90781e7cf9a035aba00914c
Nathan <nathanhello321@gmail.com>
2023-11-06 14:16:50 -0500

    tailwind now actualy actaully working? potentially. using bun run to run dev server. should incorporate supabase start into this as well. supabase is init'd i think, haven't done anything yet with it though

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -7,22 +7,21 @@ import (
 
 type resource struct {
 	url  string
-	real string
+	file string
 }
 
 var Resources = map[string]resource{
 	"output.css": {
 		url:  "/static/css/output.css",
-		real: "src/static/css/output.css",
+		file: "src/static/css/output.css",
 	},
 	"root.html": {
-		url:  "/",
-		real: "src/static/templates/root.html",
+		file: "src/static/templates/root.html",
 	},
 }
 
 func Root(w http.ResponseWriter, _ *http.Request) {
-	tmpl, err := template.ParseFiles(Resources["root.html"].real)
+	tmpl, err := template.ParseFiles(Resources["root.html"].file)
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -38,7 +37,7 @@ func Root(w http.ResponseWriter, _ *http.Request) {
 func Router() {
 	http.HandleFunc(Resources["output.css"].url, func(res http.ResponseWriter, req *http.Request) {
 		res.Header().Set("Content-Type", "text/css")
-		http.ServeFile(res, req, Resources["output.css"].real)
+		http.ServeFile(res, req, Resources["output.css"].file)
 	})
 
 	http.HandleFunc("/", Root)
