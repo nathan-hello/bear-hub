b3ca06f943c8b90f176a672b0802d46a936f7eec
Nathan <nathanhello321@gmail.com>
2024-01-10 17:10:50 +0000

    big refactor of errors in auth.go (utils and routes)

--- scripts/pg_dump.sh
+++ scripts/pg_dump.sh
@@ -6,5 +6,5 @@ cd "$dir/.."
 source .env
 touch src/db/input/remote-schema.sql
 
-pg_dump $DB_URI  --schema-only  --format=plain --enable-row-security --file src/db/input/remote-schema.sql --schema=public --schema=auth
+pg_dump $DB_URI  --schema-only  --format=plain --enable-row-security --file src/db/input/remote-schema.sql --schema=public
 
--- src/db/input/queries.sql
+++ src/db/input/queries.sql
@@ -1,23 +1,34 @@
 -- name: SelectTodos :many
-SELECT * FROM todo LIMIT $1;
-
+SELECT *
+FROM todos
+LIMIT $1;
 -- name: InsertTodo :one
-INSERT INTO todo (body) values ($1) RETURNING *;
-
+INSERT INTO todos (body)
+values ($1)
+RETURNING *;
 -- name: DeleteTodo :exec
-DELETE FROM todo WHERE id = $1;
-
+DELETE FROM todos
+WHERE id = $1;
 -- name: SelectProfileById :one
-SELECT * FROM profiles WHERE profiles.id = $1;
-
--- name: SelectUsernameFromProfileById :one
-SELECT username FROM profiles WHERE profiles.id = $1;
-
--- name: SelectProfileByUsername :one
-SELECT * FROM profiles WHERE profiles.username = $1;
-
--- name: UpdateProfileUsername :one
-UPDATE profiles SET username = $1 WHERE profiles.id = $2 RETURNING *;
-
--- name: SelectEmailAlreadyExists :one
-SELECT email FROM auth.users WHERE auth.users.email = $1;
+SELECT *
+FROM profiles
+WHERE profiles.id = $1;
+-- name: SelectEmailOrUsernameAlreadyExists :one
+SELECT email
+FROM users
+WHERE users.email = $1
+    OR users.username = $2;
+-- name: InsertUser :one
+INSERT INTO users (
+        email,
+        username,
+        encrypted_password,
+        password_created_at
+    )
+values ($1, $2, $3, $4)
+RETURNING (email, username);
+-- name: SelectUserWithEmailPassword :one
+SELECT (id)
+FROM users
+WHERE email = $1
+    AND encrypted_password = $2;
