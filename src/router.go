6e0e0d92e1faccde94f41d9424bc36a10514e827
nate <16102404+nathan-hello@users.noreply.github.com>
2024-02-08 06:00:31 +0000

    moved redirects to a single function - ssg init

--- src/router.go
+++ src/router.go
@@ -10,65 +10,6 @@ import (
 	"github.com/nathan-hello/htmx-template/src/routes"
 )
 
-type Static struct {
-	route       string
-	filepath    string
-	contentType string
-}
-
-func staticGet(filepath string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, filepath)
-	}
-}
-
-func HandlePublic() {
-
-	files := []Static{}
-
-	filepath.Walk("src/public", func(path string, info fs.FileInfo, err error) error {
-		if info.IsDir() {
-			return nil
-		}
-
-		staticRoute := path[10:]
-
-		if jsFile, err := filepath.Match("*.js", filepath.Base(path)); jsFile {
-			files = append(files, Static{route: staticRoute, filepath: path, contentType: "text/javascript"})
-			return err
-		}
-		if cssFile, err := filepath.Match("*.css", filepath.Base(path)); cssFile {
-			files = append(files, Static{route: staticRoute, filepath: path, contentType: "text/css"})
-			return err
-		}
-		imgExts := []string{".ico", ".png", ".jpg", ".webm"}
-
-		for _, v := range imgExts {
-			match, err := filepath.Match("*"+v, filepath.Base(path))
-			if err != nil {
-				return err
-			}
-			if !match {
-				continue
-			}
-			files = append(files, Static{route: staticRoute, filepath: path, contentType: ""})
-			return err
-		}
-
-		return err
-	})
-
-	defaultMiddleware := alice.New(Logging, AllowMethods("GET"))
-	for _, v := range files {
-		if v.contentType != "" {
-			defaultMiddleware.Append(CreateHeader("Content-Type", v.contentType))
-		}
-		http.Handle(v.route, defaultMiddleware.ThenFunc(staticGet(v.filepath)))
-		log.Printf("Creating route: %v, for file: %v, with Content-Type %v\n", v.route, v.filepath, v.contentType)
-	}
-
-}
-
 func HandleSites() {
 
 	type Site struct {
@@ -139,4 +80,63 @@ func HandleSites() {
 	}
 
 	http.ListenAndServe(":3000", nil)
+}
+
+type Static struct {
+	route       string
+	filepath    string
+	contentType string
+}
+
+func HandlePublic() {
+
+	files := []Static{}
+
+	filepath.Walk("src/public", func(path string, info fs.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+
+		staticRoute := path[10:]
+
+		if jsFile, err := filepath.Match("*.js", filepath.Base(path)); jsFile {
+			files = append(files, Static{route: staticRoute, filepath: path, contentType: "text/javascript"})
+			return err
+		}
+		if cssFile, err := filepath.Match("*.css", filepath.Base(path)); cssFile {
+			files = append(files, Static{route: staticRoute, filepath: path, contentType: "text/css"})
+			return err
+		}
+		imgExts := []string{".ico", ".png", ".jpg", ".webm"}
+
+		for _, v := range imgExts {
+			match, err := filepath.Match("*"+v, filepath.Base(path))
+			if err != nil {
+				return err
+			}
+			if !match {
+				continue
+			}
+			files = append(files, Static{route: staticRoute, filepath: path, contentType: ""})
+			return err
+		}
+
+		return err
+	})
+
+	middles := alice.New(Logging, AllowMethods("GET"))
+	for _, v := range files {
+		if v.contentType != "" {
+			middles.Append(CreateHeader("Content-Type", v.contentType))
+		}
+		http.Handle(
+			v.route,
+			middles.ThenFunc(
+				func(w http.ResponseWriter, r *http.Request) {
+					http.ServeFile(w, r, v.filepath)
+				}),
+		)
+		log.Printf("Creating route: %v, for file: %v, with Content-Type %v\n", v.route, v.filepath, v.contentType)
+	}
+
 }
