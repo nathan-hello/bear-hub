76b071260fc3b240c90d51ba60055f701d0e1dd9
Nathan <nathanhello321@gmail.com>
2024-05-11 17:26:22 -0400

    asdf

---
+++ src/utils/types.go
@@ -0,0 +1,10 @@
+package utils
+
+import "time"
+
+type ChatMessage struct {
+	Author  string `json:"msg-author"`
+	Text  string `json:"msg-text"`
+	Color string `json:"msg-color"`
+        CreatedAt time.Time
+}
